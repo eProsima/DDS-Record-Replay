11fcf5c89dbad7e5ad8c9f1ee37e9577abe5ae00
juanlofer-eprosima <88179026+juanlofer-eprosima@users.noreply.github.com>
2023-03-01 17:34:35 +0100

    DDS Recorder tool implementation (#26)
    * DDS Recorder tool implementation
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Uncrustify
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Sort code + new logs
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Changed status STARTED->RUNNING
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Moved files location
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Add C++17 dependency
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Remove zombie code
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Apply suggestions
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    ---------
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>

---
+++ ddsrecorder/test/resources/CommandSender.py
@@ -0,0 +1,53 @@
+# exec(open('../../../CommandSender.py').read())
+# sender = CommandSender(100)
+# sender.send("start")
+
+import fastdds
+import ControllerCommand
+
+class WriterListener (fastdds.DataWriterListener):
+    def __init__(self, writer):
+        self._writer = writer
+        super().__init__()
+
+    def on_publication_matched(self, datawriter, info):
+        if (0 < info.current_count_change):
+            print("Publisher matched subscriber {}".format(info.last_subscription_handle))
+        else:
+            print ("Publisher unmatched subscriber {}".format(info.last_subscription_handle))
+
+class CommandSender:
+    def __init__(self, domain):
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant_qos = fastdds.DomainParticipantQos()
+        factory.get_default_participant_qos(self.participant_qos)
+        self.participant = factory.create_participant(domain, self.participant_qos)
+
+        self.topic_data_type = ControllerCommand.ControllerCommandPubSubType()
+        self.topic_data_type.setName("ControllerCommand")
+        self.type_support = fastdds.TypeSupport(self.topic_data_type)
+        self.participant.register_type(self.type_support)
+
+        self.topic_qos = fastdds.TopicQos()
+        self.participant.get_default_topic_qos(self.topic_qos)
+        self.topic = self.participant.create_topic("/ddsrecorder/command", self.topic_data_type.getName(), self.topic_qos)
+
+        self.publisher_qos = fastdds.PublisherQos()
+        self.participant.get_default_publisher_qos(self.publisher_qos)
+        self.publisher = self.participant.create_publisher(self.publisher_qos)
+
+        self.listener = WriterListener(self)
+        self.writer_qos = fastdds.DataWriterQos()
+        self.publisher.get_default_datawriter_qos(self.writer_qos)
+        self.writer = self.publisher.create_datawriter(self.topic, self.writer_qos, self.listener)
+
+    def send(self, command):
+        data = ControllerCommand.ControllerCommand()
+        data.command(command)
+        self.writer.write(data)
+        print("Sending command {}".format(command))
+
+    def __del__(self):
+        factory = fastdds.DomainParticipantFactory.get_instance()
+        self.participant.delete_contained_entities()
+        factory.delete_participant(self.participant)
