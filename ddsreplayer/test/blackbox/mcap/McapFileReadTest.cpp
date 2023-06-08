10d2f23886f2bc18c6e820976c756e7ae9ced082
Irene Bandera Moreno <irenebandera@eprosima.com>
2023-06-08 11:44:22 +0200

    DDS Replay tests [18498] (#55)
    * Init commit
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Trivial working
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * All tests working
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Uncrustify
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Uncrustify
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Add replayer tests in windows
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Apply changes
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Debug
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Check if type already registered in factory
    
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    
    * Temporal
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Fix delete topic
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Debug timeout CI
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Apply changes
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Apply changes
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    * Apply changes
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    
    ---------
    
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    Co-authored-by: Juan López Fernández <juanlopez@eprosima.com>

---
+++ ddsreplayer/test/blackbox/mcap/McapFileReadTest.cpp
@@ -0,0 +1,171 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// #define MCAP_IMPLEMENTATION
+
+#include <cpp_utils/testing/gtest_aux.hpp>
+#include <gtest/gtest.h>
+
+#include "dds/HelloWorldSubscriber.h"
+
+#include "tool/DdsReplayer.hpp"
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastrtps::rtps;
+using namespace eprosima::fastrtps;
+using namespace eprosima::ddsrecorder::replayer;
+
+namespace test {
+
+// Publisher
+
+const unsigned int DOMAIN = 110;
+
+std::string topic_name = "/dds/topic";
+
+} // test
+
+
+void create_subscriber_replayer(
+        DataToCheck& data,
+        std::string configuration_path = "resources/config_file.yaml",
+        std::string input_file = "resources/helloworld_file.mcap")
+{
+    {
+        // Create Subscriber
+        HelloWorldSubscriber subscriber(
+            test::topic_name,
+            static_cast<uint32_t>(test::DOMAIN),
+            data);
+
+        std::cout << "subscriber created !!!!" << std::endl;
+        {
+            // Configuration
+            eprosima::ddsrecorder::yaml::ReplayerConfiguration configuration(configuration_path);
+            configuration.replayer_configuration->domain.domain_id = test::DOMAIN;
+
+            // Create replayer instance
+            auto replayer = std::make_unique<DdsReplayer>(configuration, input_file);
+
+            std::cout << "replayer created !!!!" << std::endl;
+
+            // Start replaying data
+            replayer->process_mcap();
+
+            std::cout << "finish process_mcap!!!!" << std::endl;
+
+            replayer->stop();
+
+            std::cout << "replayer stop!!!!" << std::endl;
+
+        }
+        // Replayer waits on destruction a maximum of wait-all-acked-timeout
+        // ms until all sent msgs are acknowledged
+        std::cout << "replayer destroyed!!!!" << std::endl;
+    }
+    std::cout << "subscriber destroyed!!!!" << std::endl;
+
+    std::cout << "process info..." << std::endl;
+
+}
+
+TEST(McapFileReadTest, trivial)
+{
+    // info to check
+    DataToCheck data;
+    create_subscriber_replayer(data);
+    ASSERT_TRUE(true);
+}
+
+TEST(McapFileReadTest, data_to_check)
+{
+    // info to check
+    DataToCheck data;
+    create_subscriber_replayer(data);
+    ASSERT_EQ(data.n_received_msgs, 11);
+    ASSERT_EQ(data.type_msg, "HelloWorld");
+    ASSERT_EQ(data.message_msg, "Hello World");
+    ASSERT_EQ(data.min_index_msg, 0);
+    ASSERT_EQ(data.max_index_msg, 10);
+    // hz ~ 200
+    ASSERT_GT(data.hz_msgs, 185);
+    ASSERT_LT(data.hz_msgs, 215);
+}
+
+TEST(McapFileReadTest, less_playback_rate)
+{
+    // info to check
+    DataToCheck data;
+    std::string configuration = "resources/config_file_less_hz.yaml";
+    create_subscriber_replayer(data, configuration);
+    // hz ~ 200
+    ASSERT_GT(data.hz_msgs, 85);
+    ASSERT_LT(data.hz_msgs, 115);
+}
+
+TEST(McapFileReadTest, more_playback_rate)
+{
+    // info to check
+    DataToCheck data;
+    std::string configuration = "resources/config_file_more_hz.yaml";
+    create_subscriber_replayer(data, configuration);
+    // hz ~ 200
+    ASSERT_GT(data.hz_msgs, 385);
+    ASSERT_LT(data.hz_msgs, 415);
+}
+
+TEST(McapFileReadTest, begin_time)
+{
+    // info to check
+    DataToCheck data;
+    std::string configuration = "resources/config_file_begin_time.yaml";
+    create_subscriber_replayer(data, configuration);
+    ASSERT_EQ(data.n_received_msgs, 3);
+    ASSERT_EQ(data.min_index_msg, 8);
+    ASSERT_EQ(data.max_index_msg, 10);
+}
+
+TEST(McapFileReadTest, end_time)
+{
+    // info to check
+    DataToCheck data;
+    std::string configuration = "resources/config_file_end_time.yaml";
+    create_subscriber_replayer(data, configuration);
+    ASSERT_EQ(data.n_received_msgs, 8);
+    ASSERT_EQ(data.min_index_msg, 0);
+    ASSERT_EQ(data.max_index_msg, 7);
+}
+
+TEST(McapFileReadTest, start_replay_time_earlier)
+{
+    // info to check
+    DataToCheck data;
+    std::string configuration = "resources/config_file_start_replay_time_earlier.yaml";
+    create_subscriber_replayer(data, configuration);
+    ASSERT_EQ(data.n_received_msgs, 11);
+    ASSERT_EQ(data.min_index_msg, 0);
+    ASSERT_EQ(data.max_index_msg, 10);
+}
+
+int main(
+        int argc,
+        char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ ddsreplayer/test/blackbox/mcap/resources/helloworld_withtypes_file_info.md
@@ -0,0 +1,83 @@
+# RECORDING
+
+## Run [publisher](https://github.com/eProsima/DDS-Record-Replay/tree/main/resources/dds/TypeLookupService)
+
+* PUBLISHER_QOS_DEFAULT
+* TOPIC_QOS_DEFAULT
+* DATAWRITER_QOS_DEFAULT
+
+```
+./TypeLookupService --entity publisher --interval 200
+```
+
+## Run DDS Recorder
+
+```
+ddsrecorder
+```
+
+# INFORMATION
+
+## TOPICS
+
+/dds/topic                      13
+
+HelloWorld
+
+    index 0
+    message "Hello World"
+
+    index 1
+    message "Hello World"
+
+    index 2
+    message "Hello World"
+
+    index 3
+    message "Hello World"
+
+    index 4
+    message "Hello World"
+
+    index 5
+    message "Hello World"
+
+    index 6
+    message "Hello World"
+
+    index 7
+    message "Hello World"
+
+    index 8
+    message "Hello World"
+
+    index 9
+    message "Hello World"
+
+    index 10
+    message "Hello World"
+
+    index 11
+    message "Hello World"
+
+    index 12
+    message "Hello World"
+
+/ddsrecorder/status             1
+
+DdsRecorderStatus
+
+    previous "CLOSED"
+    current "RUNNING"
+    info ""
+
+## START TIME
+2023-05-26
+1:48:43.944 PM CEST
+
+## END TIME
+2023-05-26
+1:48:48.029 PM CEST
+
+## DURATION
+0:00:04.085
