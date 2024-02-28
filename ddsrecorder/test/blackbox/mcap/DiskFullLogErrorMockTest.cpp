947b5d1ea6033ae3731471677bdc0787115d5db4
Lucia Echevarria <luciaechevarria@eprosima.com>
2024-02-28 10:22:50 +0100

    Add new open mcap error test
    Signed-off-by: Lucia Echevarria <luciaechevarria@eprosima.com>

--- ddsrecorder/test/blackbox/mcap/DiskFullLogErrorMockTest.cpp
+++ ddsrecorder/test/blackbox/mcap/DiskFullLogErrorMockTest.cpp
@@ -1,11 +1,15 @@
-#include <filesystem>
+#include <cpp_utils/testing/gtest_aux.hpp>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
 #include <mcap/errors.hpp>
 #include <mcap/mcap.hpp>
 
-#include <cpp_utils/testing/gtest_aux.hpp>
 #include <cpp_utils/testing/LogChecker.hpp>
+
+#include <ddsrecorder_participants/recorder/mcap/McapHandler.hpp>
+
+#include <filesystem>
 
 namespace mcap {
 namespace test {
@@ -31,6 +35,28 @@ public:
 
 } /* namespace test */
 } /* namespace mcap */
+
+namespace eprosima {
+namespace ddsrecorder {
+namespace participants {
+// Mock class for McapHandler to allow access to private members
+class MCAP_PUBLIC MockMcapHandler : public McapHandler {
+public:
+
+    MockMcapHandler(const McapHandlerConfiguration& config,
+                const std::shared_ptr<ddspipe::core::PayloadPool>& payload_pool,
+                const McapHandlerStateCode& init_state)
+    : McapHandler(config, payload_pool, init_state)
+    {
+    }
+
+    using McapHandler::open_file_nts_;
+};
+
+} /* namespace participants */
+} /* namespace ddsrecorder */
+} /* namespace eprosima */
+
 
 
 /**
@@ -77,6 +103,65 @@ TEST(DiskFullLogErrorMockTest, log_error_when_disk_is_full) {
 
     // Check if 1 LogError was captured
     ASSERT_TRUE(log_checker.check_valid());
+}
+
+/**
+ * Test case to verify logError callback when the opening mcap file fails
+ *
+ * CASES:
+ *  This test simulates a disk size available of 0 bytes and a desired writing message
+ *  of 100 bytes. As it does not have enough available space a Log Error will be displayed.
+ *  The Log Error will be captured by the Log Checker.
+ */
+TEST(DiskFullLogErrorMockTest, log_error_when_file_not_opened) {
+
+    // Create an instance of the Log Checker, in charge of capturing 1 LogError
+    eprosima::utils::testing::LogChecker log_checker(
+        eprosima::utils::Log::Kind::Error,
+        1,
+        1);
+
+    // Check no logs have been captured yet
+    ASSERT_FALSE(log_checker.check_valid());
+
+    try {
+        eprosima::ddsrecorder::participants::McapOutputSettings mcap_output_settings;
+        mcap_output_settings.output_filepath = "./fake_folder"; // This folder does not exist -> error opening file in it
+        mcap_output_settings.output_filename = "output_dummy.mcap";
+        mcap_output_settings.prepend_timestamp = false;
+        mcap_output_settings.output_timestamp_format = "%Y-%m-%d_%H-%M-%S";
+        mcap_output_settings.output_local_timestamp = true;
+
+        mcap::McapWriterOptions mcap_writer_options{"ros2"};
+
+        eprosima::ddsrecorder::participants::McapHandlerConfiguration config(
+            mcap_output_settings,
+            100,
+            1024,
+            60,
+            3600,
+            true,
+            false,
+            mcap_writer_options,
+            true,
+            false);
+
+        std::shared_ptr<eprosima::ddspipe::core::PayloadPool> payload_pool;
+        eprosima::ddsrecorder::participants::McapHandlerStateCode init_state = eprosima::ddsrecorder::participants::McapHandlerStateCode::RUNNING;
+
+        // Create an instance of the mock
+        eprosima::ddsrecorder::participants::MockMcapHandler mock_mcap_handler(config, payload_pool, init_state);
+
+        mock_mcap_handler.open_file_nts_();
+
+    } catch (const std::exception& ex) {
+        // Catch any unexpected exceptions and print their type and description
+        std::cerr << "Expected exception caught: " << typeid(ex).name() << ", " << ex.what() << std::endl;
+    }
+
+    // Check if 1 LogError was captured
+    ASSERT_TRUE(log_checker.check_valid());
+
 }
 
 int main(
