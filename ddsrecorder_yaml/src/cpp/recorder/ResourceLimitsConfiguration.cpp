984a30450ab18e2297ec45a242588f0fb028837c
Eugenio Collado <eugeniocollado@eprosima.com>
2024-11-07 08:11:15 +0100

    Regenerate types
    Signed-off-by: Eugenio Collado <eugeniocollado@eprosima.com>

--- ddsrecorder_yaml/src/cpp/recorder/ResourceLimitsConfiguration.cpp
+++ ddsrecorder_yaml/src/cpp/recorder/ResourceLimitsConfiguration.cpp
@@ -85,6 +85,7 @@ ResourceLimitsConfiguration::ResourceLimitsConfiguration(
             EPROSIMA_LOG_ERROR(YAML_READER_CONFIGURATION, "NOT VALID VALUE | SQL " << RECORDER_RESOURCE_LIMITS_SIZE_TOLERANCE_TAG << " must be greater than the minimum value accepted. Defaulting to (Mb): " << resource_limits_struct.size_tolerance_ / (1024 * 1024));
         else
             resource_limits_struct.size_tolerance_ = eprosima::utils::to_bytes(size_tolerance_str);
+        std::cout << "resource_limits_struct.size_tolerance_: " << resource_limits_struct.size_tolerance_ << std::endl;
     }
 }
 
